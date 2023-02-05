8f08b01380bf2518feea6f1b70516b387835ca3b
AlexeyBolt <boltava.av@gmail.com>
2023-02-06 02:16:17 +0300

    Домашка4

--- Homework/hw_4/intro.sql
+++ Homework/hw_4/intro.sql
@@ -9,10 +9,12 @@ CREATE TABLE EMPLOYEE (
 
 -- insert
 INSERT INTO EMPLOYEE VALUES (0001, 'Лешенька', '36', 'Краснодар');
-INSERT INTO EMPLOYEE VALUES (0002, 'Оленька', '35', 'Сочи');
-INSERT INTO EMPLOYEE VALUES (0003, 'Мистер', '18', 'Сочи');
+INSERT INTO EMPLOYEE VALUES (0002, 'Красотка Оленька', '18', 'Сочи');
+INSERT INTO EMPLOYEE VALUES (0003, 'Мистер', '28', 'Сочи');
 INSERT INTO EMPLOYEE VALUES (0004, 'Ваня', '40', 'Краснодар');
 INSERT INTO EMPLOYEE VALUES (0005, 'Алиночка', '25', 'Ёбург');
+INSERT INTO EMPLOYEE VALUES (0006, 'Персонаж 6', '32', 'Город Грехов');
+INSERT INTO EMPLOYEE VALUES (0007, 'Мишель', '32', 'Ставрополь');
 
 -- fetch 
 SELECT * FROM EMPLOYEE;
